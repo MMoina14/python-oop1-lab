1a62c4932842504a23973757a4d6e7ec6ef3f7b7
Miguel Moina <miguelamoina@gmail.com>
2025-12-12 00:20:56 -0500

    coffee

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,50 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    pass 
+    """
+    A class to represent a book in the bookstore.
     
+    Attributes:
+        title (str): The title of the book
+        page_count (int): The number of pages in the book
+    """
+    
+    def __init__(self, title, page_count):
+        """
+        Initialize a Book object.
         
+        Args:
+            title (str): The title of the book
+            page_count (int): The number of pages in the book
+        """
+        self.title = title
+        self.page_count = page_count
+    
+    @property
+    def page_count(self):
+        """Get the page count of the book."""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        """
+        Set the page count of the book.
+        
+        Args:
+            value: The page count to set
+            
+        Validates that page_count is an integer.
+        """
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+    
+    def turn_page(self):
+        """
+        Simulate turning a page in the book.
+        
+        Prints a message indicating the page has been turned.
+        """
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,52 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    pass  
+     
+    """
+    A class to represent a coffee product in the bookstore.
+    
+    Attributes:
+        size (str): The size of the coffee (Small, Medium, or Large)
+        price (float): The price of the coffee
+    """
+    
+    def __init__(self, size, price):
+        """
+        Initialize a Coffee object.
+        
+        Args:
+            size (str): The size of the coffee (Small, Medium, or Large)
+            price (float): The price of the coffee
+        """
+        self.size = size
+        self.price = price
+    
+    @property
+    def size(self):
+        """Get the size of the coffee."""
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the coffee.
+        
+        Args:
+            value (str): The size to set
+            
+        Validates that size is Small, Medium, or Large.
+        """
+        if value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+    
+    def tip(self):
+        """
+        Add a tip to the coffee price.
+        
+        Prints a thank you message and increases the price by 1.
+        """
+        print("This coffee is great, here's a tip!")  
+        self.price += 1
